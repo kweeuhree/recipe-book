fb6ee8894d71cd3b932068beea190e926ad7997c
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-13 17:49:24 -0400

    register RecipeAdmin

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Recipe
+
+class RecipeAdmin(admin.ModelAdmin):
+        list_display = ('title', 'author', 'date_created', 'date_updated')
+        search_fields = ('title', 'author')
+        list_filter = ('date_created', 'date_updated')
 
 # Register your models here.
+admin.site.register(Recipe, RecipeAdmin)
