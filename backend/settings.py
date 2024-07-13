a98b82fa49cdf9fade10b9e0f9039fa6487395ec
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-13 17:27:21 -0400

    troubleshoot .env installation

--- backend/settings.py
+++ backend/settings.py
@@ -10,10 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv()
 import os
+from pathlib import Path
+
+# load environment variables
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,11 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+        'localhost'
+]
 
 
 # Application definition
