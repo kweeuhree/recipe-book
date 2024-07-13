a060060dca5e1548c412455a7bd1636fc746c4c3
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-13 17:35:05 -0400

    register Note model

--- note/admin.py
+++ note/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Todo
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('date', 'body', 'isLiked')
 
 # Register your models here.
+
+admin.site.register(Note, NoteAdmin)
