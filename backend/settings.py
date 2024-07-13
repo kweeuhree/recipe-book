edf955ece3bff5333a97dd2b063d5246d1e38b6c
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-13 19:03:32 -0400

    update settings.py to add localhost to cors origin whitelist

--- backend/settings.py
+++ backend/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'recipe',
     'note',
 ]
@@ -56,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -130,3 +133,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
+
+# Allow credentials (cookies, Authorization headers, etc.) to be included in cross-origin requests
+CORS_ALLOW_CREDENTIALS = True
