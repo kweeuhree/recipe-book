c08f1fb91ee5f62c98073b3fc56531eb83f2559c
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-13 17:33:13 -0400

    create Note model and migrate

--- note/models.py
+++ note/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    date = models.DateField(max_length=120)
+    body = models.TextField()
+    isLiked = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.title
