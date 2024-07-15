5526c3fc1b8575e112215338366988a083b89fe1
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-15 10:35:50 -0400

    add isLiked to serializers.py, add more recipes

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -3,7 +3,8 @@ from note.serializers import NoteSerializer
 from recipe.models import Recipe
 
 class RecipeSerializer(serializers.ModelSerializer):
+    isLiked = serializers.BooleanField(default=False)
     notes = NoteSerializer(many=True, read_only=True)
     class Meta:
         model = Recipe
-        fields = ('id', 'title', 'description', 'ingredients', 'instructions', 'servings', 'date_created', 'date_updated', 'image', 'notes')
+        fields = ('id', 'title', 'description', 'ingredients', 'instructions', 'servings', 'date_created', 'date_updated', 'image', 'notes', 'isLiked')
--- recipe/views.py
+++ recipe/views.py
@@ -16,12 +16,21 @@ class RecipeView(viewsets.ModelViewSet):
 
 @api_view(['POST'])
 def like_recipe(request, recipe_id):
-    recipe = get_object_or_404(Recipe, id=recipe_id)
-    if recipe.isLiked:
-        return Response({'status': 'already liked'}, status=400)
-    recipe.isLiked = True
-    recipe.save()
-    return Response({'status': 'liked'})
+    try:
+        print(recipe_id, 'id in like_recipe')
+        recipe = get_object_or_404(Recipe, id=recipe_id)
+        print(recipe.isLiked, 'recipe.isLiked in like_recipe')
+        if recipe.isLiked:
+            return Response({'status': 'already liked'}, status=400)
+        
+        recipe.isLiked = True
+        recipe.save()
+        print(recipe.isLiked, 'recipe.isLiked after saving')
+        return Response({'status': 'liked'})
+    
+    except Exception as e:
+        print(f"Error liking recipe: {e}")
+        return Response({'status': 'error'}, status=500)
 
 @api_view(['POST'])
 def unlike_recipe(request, recipe_id):
