4a7f1de34c954a3be4f0bd1c1396f038c79236e5
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-17 17:02:28 -0400

    troubleshoot routing

--- backend/views.py
+++ backend/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import redirect
 
 
 def home_page(request):
-    return redirect('http://django-react-bucket.s3.amazonaws.com/index.html')
+    return redirect('http://django-react-bucket.s3.amazonaws.com/')
