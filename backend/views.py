da614514e595bac4107355f007275d856a98b73f
Nika Kolesnikova <firstnamenika@gmail.com>
2024-07-17 15:24:20 -0400

    typo

--- backend/views.py
+++ backend/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import redirect
 
 
 def home_page(request):
-    return redirect('https://your-s3-bucket-url.s3.amazonaws.com/index.html')
+    return redirect('https://django-react-bucket.s3.amazonaws.com/index.html')
